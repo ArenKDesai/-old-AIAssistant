b40e9200e62638459b5ca07ec80ab2379629b9e1
Aren <flare20012@gmail.com>
2024-05-10 11:56:42 -0500

    building wheels

--- audio.py
+++ audio.py
@@ -1,4 +1,4 @@
-# import pyaudio
+import pyaudio
 # import wave
 # import threading
 # import whisper
@@ -6,44 +6,44 @@ from convo_processing import get_response
 import requests
 from KEYS import elabs_api, voice_api
 
-# def record_audio(model, duration=3, rate=44100, chunk=1024, channels=2, format=pyaudio.paInt16):
-#     p = pyaudio.PyAudio()
+def record_audio(model, duration=3, rate=44100, chunk=1024, channels=2, format=pyaudio.paInt16):
+    p = pyaudio.PyAudio()
 
-#     stream = p.open(format=format,
-#                     channels=channels,
-#                     rate=rate,
-#                     input=True,
-#                     frames_per_buffer=chunk)
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=chunk)
 
-#     frames = []
+    frames = []
 
-#     for i in range(0, int(rate / chunk * duration)):
-#         data = stream.read(chunk)
-#         frames.append(data)
+    for i in range(0, int(rate / chunk * duration)):
+        data = stream.read(chunk)
+        frames.append(data)
 
-#     stream.stop_stream()
-#     stream.close()
-#     p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-#     wf = wave.open("recording.wav", 'wb')
-#     wf.setnchannels(channels)
-#     wf.setsampwidth(p.get_sample_size(format))
-#     wf.setframerate(rate)
-#     wf.writeframes(b''.join(frames))
-#     wf.close()
+    wf = wave.open("recording.wav", 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 
-# def main_audio_loop():
-#     model = whisper.load_model("medium")
+def main_audio_loop():
+    model = whisper.load_model("medium")
 
-#     while True:
-#         record_audio(model)
-#         convo = whisper.listen("recording.wav").lower()
-#         if 'pluto' in convo:
-#             while convo:
-#                 tag = process_convo(convo)
-#                 response = "" # TODO: get gpt's response
-#                 # TODO: 
+    while True:
+        record_audio(model)
+        convo = whisper.listen("recording.wav").lower()
+        if 'pluto' in convo:
+            while convo:
+                tag = process_convo(convo)
+                response = "" # TODO: get gpt's response
+                # TODO: 
 
 def speak(response):
     CHUNK_SIZE = 1024
@@ -70,4 +70,4 @@ def speak(response):
             if chunk:
                 f.write(chunk)
 
-speak(get_response("Make fun of my friend Brianna."))
+speak(get_response("I'm bored"))
